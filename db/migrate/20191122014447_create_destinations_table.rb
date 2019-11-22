1b617cbec25dd56d647e3ee781d15f7fc74174bc
Gigi Gamao <mmgamao1@gmail.com>
2019-11-22 02:00:17 +0000

    edit migration file

--- db/migrate/20191122014447_create_destinations_table.rb
+++ db/migrate/20191122014447_create_destinations_table.rb
@@ -1,4 +1,14 @@
-class CreateDestinationsTable < ActiveRecord::Migration
+class CreateDestinationsTable < ActiveRecord::Migration[6.0]
   def change
+    create_table :destinations do |d|
+      t.string :restaurants
+      t.string :nightlife
+      t.string :nature_and_parks
+      t.string :landmarks_and_monuments
+      t.string :museums
+      t.string :city 
+      t.string :country
+      t.string :year_visited
+    end
   end
 end
